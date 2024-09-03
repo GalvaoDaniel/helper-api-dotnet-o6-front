5c7719d21afe6e7f04f2f8578d3ca760354a609d
Daniel Galvão <galvaoazevedod@gmail.com>
2024-09-02 23:08:42 -0300

    Chamada as APIS
    Código com problemas - temporário

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Typography,
@@ -8,15 +8,46 @@ import {
   Paper,
 } from '@mui/material';
 import { Chart } from 'react-google-charts';
+import axios from 'axios';
 
 // Defina a taxa CDI (use uma média ou valor atual para cálculos)
 const CDI_RATE = 0.13; // Exemplo: 13% ao ano
+
+interface Taxas {
+  cdiUltimoMes: string;
+  cdiMedioMensal: string;
+  cdiUltimosAno: string;
+}
+
+interface CalculoInvestimentoRequest {
+  cdiAnual: number;
+  cdiMensal: number;
+  valor: number;
+  porcentagemCdi: number;
+  dataFim: string;
+}
+
+interface CalculoInvestimentoResponse {
+  valorCdi: number;
+  valorIpca: number;
+  valorSelic: number;
+}
 
 const App: React.FC = () => {
   const [initialValue, setInitialValue] = useState<number>(0);
   const [years, setYears] = useState<number>(0);
   const [result, setResult] = useState<number | null>(null);
   const [chartData, setChartData] = useState<any[]>([]);
+
+  const [taxas, setTaxas] = useState<Taxas | null>(null);
+  const [calculoRequest, setCalculoRequest] = useState<CalculoInvestimentoRequest>({
+    cdiAnual: 0,
+    cdiMensal: 0,
+    valor: 0,
+    porcentagemCdi: 0,
+    dataFim: new Date().toISOString(), // Data atual
+  });
+  const [calculoResponse, setCalculoResponse] = useState<CalculoInvestimentoResponse | null>(null);
 
   const calculateReturn = () => {
     const data = [['Ano', 'CDI', 'Aleatório 1', 'Aleatório 2']];
@@ -35,8 +66,52 @@ const App: React.FC = () => {
     console.log({data});
 
     // Calcula o valor final após o período especificado
-    const finalAmount = initialValue * Math.pow(1 + CDI_RATE, years);
-    setResult(finalAmount);
+    // const finalAmount = initialValue * Math.pow(1 + CDI_RATE, years);
+    // setResult(finalAmount);
+
+    calcularInvestimento().then(success => {
+      console.log("Calculo do Investimento realizado com SUCESSO: " + success)
+      setResult(calculoResponse ? calculoResponse.valorCdi : null);
+    }).catch(error => {
+      console.log("Erro ao chamar a API para calcular o valor Investido: " + error)
+    });
+
+  };
+
+  // Função para buscar as taxas (GET request)
+  useEffect(() => {
+    const fetchTaxas = async () => {
+      try {
+        const response = await axios.get<Taxas>('http://localhost:5156/investimento/v1/obter-taxas');
+        setTaxas(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar as taxas:', error);
+      }
+    };
+
+    fetchTaxas();
+  }, []);
+
+  // Função para calcular investimento (POST request)
+  const calcularInvestimento = async () => {
+    const currentDate = new Date();
+    setCalculoRequest({
+      cdiAnual: taxas ? Number(taxas.cdiUltimosAno) : 0,
+      cdiMensal: taxas ? Number(taxas.cdiMedioMensal) : 0,
+      valor: initialValue,
+      porcentagemCdi: 110,      // TODO: Mudar esse valor! É apenas para teste!!
+      dataFim: new Date(currentDate.setFullYear(currentDate.getFullYear() + 10)).toISOString()
+    });
+
+    try {
+      const response = await axios.post<CalculoInvestimentoResponse>(
+        'http://localhost:5156/investimento/v1/calcular-investimento',
+        calculoRequest
+      );
+      setCalculoResponse(response.data);
+    } catch (error) {
+      console.error('Erro ao calcular investimento:', error);
+    }
   };
 
   return (
